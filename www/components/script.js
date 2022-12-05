edcc6a031253eeacfc98a0f4a7714b8dd763d215
emanueBolinho <emanuellegaya675@gmail.com>
2022-12-05 23:31:19 +0900

    AAAAAAAAA

--- www/components/script.js
+++ www/components/script.js
@@ -1,22 +1,34 @@
-window.onload = function(){
-    var url = 'http://hakim.profrodolfo.com.br/receitas.php/cadastroU.php';
-    var nome = document.querySelector('#nome');
-    var email = document.querySelector('#email');
-    var data = document.querySelector('#dt_nasc');
-    var senha = document.querySelector('#senha');
 
-    var formData = new FormData();
-    formData.append('nome', nome.value);
-    formData.append('email', email.value);
-    formData.append('dt_nasc', data.value);
-    formData.append('senha', senha.value);
 
-    fetch(url, { method: 'POST', body: formData})
-    .then(function (response) {
-        alert(response);
+//window.onload = function(){
+    
+    // var url = 'http://hakim.profrodolfo.com.br/receitas.php/cadastroU.php';
+    // var nome = document.querySelector('#nome');
+    // var email = document.querySelector('#email');
+    // var data = document.querySelector('#dt_nasc');
+    // var senha = document.querySelector('#senha');
+
+    // var formData = new FormData();
+    // formData.append('nome', nome.value);
+    // formData.append('email', email.value);
+    // formData.append('senha', senha.value);
+    // formData.append('dt_nasc', data.value);
+    
+    //btn.addEventListener('click', async ()=>{
+  /*  btn.addEventListener('click', function(){
+        //let data = await fetch(url, {method: 'POST', body:formData});
+        
+        fetch(url, { 
+        method: 'POST', 
+        body: formData}
+    .then(function (res) {
+        //alert('a');
+        return res.text();
     })
-    .then(function (body) {
-        alert(body);
-    });
-}
-
+    .then(function(data){
+         alert(data);        
+    //});
+//));*/
+//});
+//}
+    
